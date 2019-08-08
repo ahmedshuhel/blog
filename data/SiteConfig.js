eac4f2b81b0f3536237dcd00df389eba635de2bb
Shuhel Ahmed <ahmedshuhel@newscred.com>
2019-08-08 17:14:43 +0600

    Update copyright

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -9,7 +9,7 @@ module.exports = {
   siteUrl: "https://blog.ahmedshuhel.com", // Domain of your website without pathPrefix.
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-casper/.
   siteDescription:
-    "A Casper themed GatsbyJS stater based on the Advanced Starter.", // Website description used for RSS feeds/meta description tag.
+    "Random thougths on Programming, Software Engineering and Life", // Website description used for RSS feeds/meta description tag.
   siteCover:
     "https://haysclark.github.io/gatsby-starter-casper/images/blog-cover.jpg", // Optional, the cover image used in header for home page. e.g: "/images/blog-cover.jpg",
   siteNavigation: true, // If navigation is enabled the Menu button will be visible
@@ -45,9 +45,9 @@ module.exports = {
   ],
   // Copyright string for the footer of the website and RSS feed.
   copyright: {
-    label: "Gatsby Casper Starter" // Label used before the year
-    // year: "2018" // optional, set specific copyright year or range of years, defaults to current year
-    // url: "https://www.gatsbyjs.org/" // optional, set link address of copyright, defaults to site root
+    label: "Shuhel Ahmed", // Label used before the year
+    year: "2019", // optional, set specific copyright year or range of years, defaults to current year
+    url: "https://blog.ahmedshuhel.com/" // optional, set link address of copyright, defaults to site root
   },
   themeColor: "#c62828", // Used for setting manifest and progress theme colors.
   backgroundColor: "#e0e0e0", // Used for setting manifest background color.
